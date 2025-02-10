ba2d4d187518912fb33a12a02b93b9820bbe67f2
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2025-02-10 14:01:35 -0300

    Initialize project with input prompt

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(@"
+Digite uma palavra que atenda as seguintes condições:
+ - Respeite qualquer um dos formatos:
+    - GOOGLE, google ou Google
+ - Só contenha letras maiúsculas e/ou minúsculas
+ - Tenha de 1 a 100 caracteres
+");
+
+var word = Console.ReadLine() ?? string.Empty;
