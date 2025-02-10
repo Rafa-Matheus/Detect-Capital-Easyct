516d95a01a512a28577e56a7f11f5d4be942a857
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2025-02-10 14:11:26 -0300

    Validate user input before processing

--- Program.cs
+++ Program.cs
@@ -9,6 +9,23 @@ Digite uma palavra que atenda as seguintes condições:
 var word = Console.ReadLine() ?? string.Empty;
 
 
+var (isWordValid, errorMessage) = InputValidation(word);
+
+if (!isWordValid)
+{
+    Console.WriteLine($"Erro: {errorMessage}");
+}
+else
+{
+    var solution = new Solution();
+    string message = solution.DetectCapitalUse(word)
+        ? $"A palavra \"{word}\" ATENDE as condições sobre o uso adequado de letras maiúsculas."
+        : $"A palavra \"{word}\" NÃO ATENDE as condições sobre o uso adequado de letras maiúsculas.";
+
+    Console.WriteLine(message);
+}
+
+
 static (bool isValid, string errorMessage) InputValidation(string word)
 {
     if (word.Length < 1 || word.Length > 100)
