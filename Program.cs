c625c88c7f32cd4e38fbc9ef18dbdba217acef66
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2025-02-10 14:05:48 -0300

    Add input validation function

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ Digite uma palavra que atenda as seguintes condições:
 ");
 
 var word = Console.ReadLine() ?? string.Empty;
+
+
+static (bool isValid, string errorMessage) InputValidation(string word)
+{
+    if (word.Length < 1 || word.Length > 100)
+        return (isValid: false, errorMessage: "A palavra deve conter de 1 a 100 caracteres.");
+    else if (!word.All(char.IsLetter))
+        return (isValid: false, errorMessage: "A palavra deve conter apenas letras.");
+    else
+        return (isValid: true, errorMessage: string.Empty);
+}
